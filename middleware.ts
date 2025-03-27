5e423a38b46e7eaef80731f183e62ae93081b8f2
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2025-03-27 22:51:54 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-    const url = req.nextUrl;
-    const host = req.headers.get('host');
+  const url = req.nextUrl;
+  const host = req.headers.get('host');
 
-    if (host?.includes('admin.examina.live')) {
-        url.pathname = `/admin${url.pathname}`;
-        return NextResponse.rewrite(url);
-    }
+  if (host?.includes('examina.live') && url.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
 
-    return NextResponse.next();
+  if (host?.includes('admin.examina.live')) {
+    url.pathname = `/admin${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
 }
 
+// Apply middleware to all routes
 export const config = {
-    matcher: '/:path*',
+  matcher: '/:path*',
 };
