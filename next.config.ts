01c52b7a977772c2b10364ece30e0dd14b51b9d8
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2025-03-27 23:21:49 +0530

    2

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,42 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' 
+    ? 'https://admin.examina.live' 
+    : '/',
+
+  webpack: (config, { }) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback, 
+      fs: false 
+    };
+
+    return config;
+  },
+
   async rewrites() {
     return [
       {
-        source: '/login',
-        has: [{ type: 'host', value: 'examina.live' }],
-        destination: '/login',
+        source: '/static/:path*',
+        destination: '/static/:path*',
       },
+      {
+        source: '/_next/static/:path*',
+        destination: '/_next/static/:path*',
+      }
+    ];
+  },
+
+  async redirects() {
+    return [
       {
         source: '/admin/:path*',
         has: [{ type: 'host', value: 'admin.examina.live' }],
         destination: '/admin/:path*',
-      },
+        permanent: false,
+      }
     ];
-  },
+  }
 };
 
 export default nextConfig;
