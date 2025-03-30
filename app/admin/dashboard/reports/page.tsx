61cffd571b36d5dff0787223650b1029100b1412
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2025-03-30 18:14:49 +0530

    EXM-26 report content

---
+++ app/admin/dashboard/reports/page.tsx
@@ -0,0 +1,12 @@
+import Reports from '@/components/admin/Dashboard/reports/report'
+
+const ReportTab = () => {
+    return (
+      <div>
+        <Reports />
+      </div>
+    );
+  };
+  
+  export default ReportTab;
+  
