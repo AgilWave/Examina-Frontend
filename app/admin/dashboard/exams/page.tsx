ececf3ee1bba29bf252942bcf029a724b99dcc94
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2025-03-30 14:12:18 +0530

    EXM-23 exam content view

---
+++ app/admin/dashboard/exams/page.tsx
@@ -0,0 +1,13 @@
+import Exams from '@/components/admin/ExamContent/ExamContent'
+
+const ExamContent = () => {
+    return (
+      <div>
+        <Exams />
+      </div>
+    );
+  };
+
+  
+  export default ExamContent;
+  
