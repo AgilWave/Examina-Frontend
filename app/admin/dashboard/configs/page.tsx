08d8519d4d836ad28299de01970d8e75862dbc02
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2025-03-30 18:34:43 +0530

    EXM-27 config content

---
+++ app/admin/dashboard/configs/page.tsx
@@ -0,0 +1,12 @@
+import Configurations from '@/components/admin/Dashboard/configuration/configuration'
+
+const ConfigurationsTab = () => {
+    return (
+      <div>
+        <Configurations />
+      </div>
+    );
+  };
+  
+  export default ConfigurationsTab;
+  
