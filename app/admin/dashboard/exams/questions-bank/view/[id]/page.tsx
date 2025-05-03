a0e0622cb8fc545ea433a383ef0f2a41c9f3b56a
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2025-05-03 19:19:58 +0530

    feat(dynamc): fix dynamic route

--- app/admin/dashboard/exams/questions-bank/view/[id]/page.tsx
+++ app/admin/dashboard/exams/questions-bank/view/[id]/page.tsx
@@ -1,41 +1,11 @@
-import QuestionBank from "@/components/admin/Dashboard/ExamContent/questionBank/viewQuestionBank/index";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import QuestionBankWrapper from "@/components/wrapper/QuestionBankWrapper";
 
 export const dynamic = 'force-dynamic';
 
-const ViewQuestionBankPage = async ({
-  params,
-}: {
-  params: { id: string };
-}) => {
-  const id = parseInt(params.id, 10);
-
-  if (!id || isNaN(id)) {
-    return (
-      <div className="flex flex-col justify-center items-center h-[88vh] bg-card px-4 text-center">
-        <div className="max-w-md space-y-6">
-          <h1 className="text-3xl font-extrabold text-foreground">
-            Invalid Question Bank ID
-          </h1>
-          <p className="text-muted-foreground">
-            The ID you provided is either missing or invalid.
-          </p>
-          <Button asChild>
-            <Link href="/admin/dashboard/exams/questions-bank">
-              Go to Question Bank
-            </Link>
-          </Button>
-        </div>
-      </div>
-    );
-  }
-
+export default function ViewQuestionBankPage() {
   return (
     <div>
-      <QuestionBank />
+      <QuestionBankWrapper />
     </div>
   );
-};
-
-export default ViewQuestionBankPage;
+}
