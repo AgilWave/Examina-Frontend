fe6a930d5d286383373969525b58f05791ff165d
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2025-05-07 03:23:56 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -161,10 +161,10 @@ export async function middleware(req: NextRequest) {
 
     // THEN: Allow /api to pass through
     if (url.pathname.startsWith("/api")) {
-      console.log("API request detected, allowing through middleware.");
-      console.log("Request URL:", req.url);
-      console.log("Request Pathname:", url.pathname);
-      return NextResponse.next();
+      const newPath = url.pathname.replace("/api", "/lecturer/api");
+      const newUrl = new URL(newPath, req.url);
+      console.log(`Rewriting /api to ${newUrl}`);
+      return NextResponse.rewrite(newUrl);
     }
 
     if (!lecturerJwt) {
