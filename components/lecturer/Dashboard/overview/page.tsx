d754487cb45644532ebe5135012aff756351a71c
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2025-05-14 16:01:28 +0530

    Update page.tsx

--- components/lecturer/Dashboard/overview/page.tsx
+++ components/lecturer/Dashboard/overview/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { JSX } from "react";
+import React from "react";
 import { ReactNode } from "react";
 import {
   Breadcrumb,
@@ -16,11 +16,6 @@ import {
   Users,
   Hourglass,
   MoveUp,
-  FileText,
-  ClipboardList,
-  Database,
-  User,
-  Laptop,
 } from "lucide-react";
 
 import { Card, CardContent } from "@/components/ui/card";
@@ -34,10 +29,6 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 
-interface upcomingActivity {
-  id: string;
-  time: string;
-}
 
 interface StatCardProps {
   title: string;
@@ -92,145 +83,6 @@ const StatCard: React.FC<StatCardProps> = ({
   );
 };
 
-const getStatusColor = (status: string) => {
-  switch (status.toLowerCase()) {
-    case "participated":
-      return "text-blue-600 bg-blue-500/20";
-    case "upcoming":
-      return "text-teal-400 bg-teal-500/20";
-    default:
-      return "text-gray-400 bg-gray-500/20";
-  }
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const renderIcon = (icon: string) => {
-  return (
-    <div className="flex items-center justify-center w-10 h-10 rounded-full bg-teal-600 text-white">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-        <polyline points="14 2 14 8 20 8"></polyline>
-        <line x1="16" y1="13" x2="8" y2="13"></line>
-        <line x1="16" y1="17" x2="8" y2="17"></line>
-        <polyline points="10 9 9 9 8 9"></polyline>
-      </svg>
-    </div>
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ExamHistory = ({ activities = sampleActivities }) => {
-  return (
-    <div className="space-y-4 max-h-[300px] overflow-y-auto scrollbar-custom pr-2 custom-scrollbar">
-      {activities.map((activity) => (
-        <div
-          key={activity.id}
-          className="flex flex-col sm:flex-row sm:justify-between gap-4 bg-[#F6F6F6] dark:bg-black/80 rounded-xl p-4 dark:border border-teal-600/50 hover:border-teal-500 hover:bg-gray-200 dark:hover:bg-black/90 transition-all duration-300 backdrop-blur-sm"
-        >
-          {/* Left Section: Icon + Course + Date */}
-          <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4">
-            <div className="flex items-center gap-2">
-              {renderIcon(activity.icon)}
-              <span className="font-medium text-gray-800 dark:text-white">
-                {activity.course}
-              </span>
-            </div>
-            <div className="text-left sm:text-center mt-2 sm:mt-0">
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                {activity.date}
-              </span>
-            </div>
-          </div>
-
-          {/* Right Section: Badges */}
-          <div className="flex flex-row flex-wrap items-center gap-2 sm:gap-3">
-            <span className="text-sm text-green-600 bg-green-500/20 rounded-full px-3 py-1 font-medium w-fit">
-              {activity.type}
-            </span>
-            <span
-              className={`text-sm rounded-full px-3 py-1 font-medium w-fit ${getStatusColor(
-                activity.status
-              )}`}
-            >
-              {activity.status}
-            </span>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const iconMap: { [key: string]: JSX.Element } = {
-  "Upload Course work": <FileText className="w-5 h-5 text-white" />,
-  "View Results": <ClipboardList className="w-5 h-5 text-white" />,
-  "Course Work Details": <BookOpen className="w-5 h-5 text-white" />,
-  "Data Management Exam": <Database className="w-5 h-5 text-white" />,
-  "View profile": <User className="w-5 h-5 text-white" />,
-  "Online Exam": <Laptop className="w-5 h-5 text-white" />,
-};
-
-
-const sampleActivities = [
-  {
-    id: 1,
-    course: "Data Management",
-    date: "12/03/2025",
-    type: "Online Exam",
-    status: "Participated",
-    icon: "document",
-  },
-  {
-    id: 2,
-    course: "Data Management",
-    date: "12/03/2025",
-    type: "Course Work",
-    status: "Upcoming",
-    icon: "document",
-  },
-  {
-    id: 3,
-    course: "Software Engineering",
-    date: "12/03/2025",
-    type: "Online Exam",
-    status: "Participated",
-    icon: "document",
-  },
-  {
-    id: 4,
-    course: "EAD2",
-    date: "12/03/2025",
-    type: "Online Exam",
-    status: "Participated",
-    icon: "document",
-  },
-  {
-    id: 5,
-    course: "Data Management",
-    date: "12/03/2025",
-    type: "Online Exam",
-    status: "Participated",
-    icon: "document",
-  },
-  {
-    id: 6,
-    course: "Data Management",
-    date: "12/03/2025",
-    type: "Course Work",
-    status: "Upcoming",
-    icon: "document",
-  },
-];
 
 
 export default function AdminDashboard() {
